ff46eefb22d68b8f9d282e358cc2d4cef9c9fbca
iresuji <iresuji@hotmail.com>
2023-01-08 23:53:07 +0000

    generate password options updated

--- js/script.js
+++ js/script.js
@@ -90,44 +90,80 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  //Password length prompt
+
+  let passwordLength = parseInt(prompt("Enter password length between 10 & 64 characters"));
+  if (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
+    alert("Password lenght must be between 10 & 64 characters");
+    return null;
+  }
+
+  console.log(passwordLength);
+
+  let passwordUpperCase = confirm("Do you want Uppercase in your password?");
+  let passwordLowerCase = confirm("Do you want Lowercase in your password?");
+  let passwordNumbers = confirm("Do you want Numbers in your password?");
+  let passwordSpecial = confirm("Do you want Special characters in your password?");
 
 }
+const passwordObject = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  number: getRandomNumeric,
+  special: getRandomSpecial,
+}
+//console.log(passwordObject);
 
-//Password lenght
+//Generator functions
 
-let passwordLenght = prompt("Enter password length between 10 & 64 characters");
-if (passwordLenght < 10 || passwordLenght > 64 || isNaN(passwordLenght)) {
-  passwordLenght = prompt("Enter password length between 10 & 64 characters");
-  else {
-    passwordLenght = prompt("Password lenght must be between 10 & 64 characters");
+function getRandomUpper() {
+  if (passwordUpperCase) {
+    upperCaseArray = characterArray.concat(upperCasedCharacters);
+    return upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
   }
 }
 
-//Uppercase
+console.log(getRandomUpper());
 
-let passwordUpperCase = confirm("Do you want Uppercase in your password?");
+function getRandomLower() {
+  return lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
+}
 
+function getRandomNumeric() {
+  return numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+}
 
-//Uppercase
-
-let passwordLowerCase = confirm("Do you want Lowercase in your password?");
-
-//Numeric
-
-let passwordNumeric = confirm("Do you want to include numbers in your password?");
-
-//Special characters
-
-let passwordSpecialCharacters = confirm("Do you want to include special characters in your password?");
-
-function getRandom(arr) {
-
+function getRandomSpecial() {
+  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(upper, lower, number, special, passwordLength) {
+  // Initialize password variables
 
+  //Filter out unchecked types
+
+  //Loop over length
+
+
+
+
+
+  let characterArray = [upper + lower + number + special];
+
+  let finalPassword = '';
+  let options = getPasswordOptions();
+  console.log(getPasswordOptions());
+  // if (options.passwordUpperCase) {
+  //   upperCaseArray = characterArray.concat(upperCasedCharacters);
+  // }
+  // //const length = passwordLength.value;
+  // //console.log(length);
 }
+
+// return finalPassword;
+//console.log(getRandom(options));
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
